b6cd26ec3ee016e34716504fcb387bdb2d60c568
khalid RAFIK <khalid.rfk@gmail.com>
2021-09-25 23:58:13 +0100

    static i18n

--- src/task-runners/gulp.ts
+++ src/task-runners/gulp.ts
@@ -4,9 +4,13 @@ import Vinyl from "vinyl";
 import {readFileSync} from 'fs';
 import ts from "typescript";
 import type {Transform} from 'stream';
+import EJS from 'ejs';
+import Glob from 'glob';
+import {createRequire} from 'module';
+const require= createRequire(import.meta.url);
 
 /** Adapter for gulp */
-export function createGulpPipe(tsConfig: string|ts.CompilerOptions, pretty=true){
+export function gulp(tsConfig: string|ts.CompilerOptions, pretty=true){
 	const files: Map<string, Vinyl>= new Map();
 	if(typeof tsConfig==='string') tsConfig= parseTsConfig(tsConfig);
 	function collect(file: Vinyl, _:any, cb: Through.TransformCallback){
@@ -31,4 +35,72 @@ export function parseTsConfig(tsConfigPath: string){
 	var tsP2= ts.convertCompilerOptionsFromJson(tsP.config.compilerOptions, process.cwd(), tsConfigPath);
 	if(tsP2.errors?.length) throw new Error("Config file parse fails:" + tsP2.errors.map(e=> e.messageText.toString()));
 	return tsP2.options;
+}
+
+/**
+ * Prepare views for final compiling
+ * Apply i18n & precompiling logic
+ */
+export async function gulpInitViews<T>(
+	/** Glob selector to compiled time i18n files */
+	i18nGlobPattern: string,
+	/** Aditional data to the compiler */
+	data?: T,
+	/** i18n var name inside files */
+	i18nVarname: string= 'i18n',
+	/** Custom Compiler */
+	render: (content: string, data: T)=> string= _ejs
+){
+	// Load i18n files
+	var i18n= _resolveI18n(i18nGlobPattern, i18nVarname);
+	// data
+	data= {...data, i18n} as any as T;
+	// Executor
+	return Through.obj(function(file: Vinyl, _:any, cb: Through.TransformCallback){
+		var err: any= null;
+		try{
+			// Exclude streams
+			if(file.isStream()) throw new Error(`Streams are not supported. Input file: ${file.path}`);
+			// Get content
+			var content= file.isBuffer()? file.contents.toString('utf-8') : readFileSync(file.path, 'utf-8');
+			// Compile content
+			content= render(content, data!);
+			// Save
+			file.contents= Buffer.from(content);
+		}catch(e){
+			err= e ?? 'ERROR!';
+		}
+		cb(err, file);
+	});
+}
+
+
+/** EJS compiler */
+function _ejs(content: string, data: Record<string, any>){
+	return EJS.render(content, data)
+}
+
+/** Resolve i18n */
+function _resolveI18n(globPattern: string, i18nVarname:string){
+	// Load i18n files
+	var files= Glob.sync(globPattern);
+	var len= files.length;
+	if(len===0){
+		throw new Error(`No file found for pattern: ${globPattern}`)
+	}
+	// Load locales
+	const requireCache= require.cache;
+	const i18nMap: Map<string, Record<string, any>>= new Map();
+	for(let i=0; i<len; ++i){
+		let file= files[i];
+		if(requireCache.hasOwnProperty(file)) requireCache[file]= undefined;
+		let m= require(file)?.[i18nVarname];
+		let locale= m?.locale;
+		if(typeof locale !== 'string')
+			throw new Error(`Missing "${i18nVarname}.locale" inside file: ${file}`);
+		if(i18nMap.has(locale))
+			throw new Error(`Duplicated locale "${locale}". Last one in: ${file}`);
+		i18nMap.set(locale, m);
+	}
+	return i18nMap;
 }
